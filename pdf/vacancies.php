bbe3bc6c2395532e6c5adee066ae819d8b306675
kipsang2019 <KIPSANGMOSES15@students.tukenya.ac.ke>
2019-03-28 17:58:27 +0300

    added county.gif and pdf/vacanciees.php to the project

---
+++ pdf/vacancies.php
@@ -0,0 +1,42 @@
+<?php 
+	
+	//dbs connection
+	$conn = mysqli_connect('localhost', 'root', '', 'e-governance');
+
+
+	//select query
+	$sql = "SELECT * FROM uploadjobs";
+	$results = mysqli_query($conn, $sql);
+
+	require ('fpdf181/fpdf.php');
+
+
+	$pdf = new FPDF('p', 'mm', 'A3');
+	$pdf -> AddPage();
+	$pdf -> SetFont('Arial','B','12');
+	$pdf -> SetTextColor(25, 77, 51);
+	$pdf -> SetFillColor(230, 230, 230);
+	$pdf -> Image('..\images/county.gif',90,75,50,0);
+	$pdf -> Cell(210,10, 'COUNTY GOVERNMENT OF TRANS-NZOIA', 0, 1, 0, 'C');
+	$pdf -> Cell(210,10, 'P.O. Box 4211-30200, Kitale', 0, 1, 0, 'C');
+	$pdf -> Cell(210,10, 'Avalilable vacancies', 0, 1, 0, 'C');
+
+	//$pdf -> Cell(w, h, txt, border, ln, align, fill, link)
+	$pdf -> SetTextColor(0, 0, 0);
+	$pdf -> SetFillColor(160,160,160);
+	$pdf -> Cell(100,10, 'Designation', 1, 0, 0, 'B','C');
+	$pdf -> Cell(30,10, 'Scale', 1, 0, 0, 'B','C');
+	$pdf -> Cell(30,10, 'Post NO', 1, 0, 0, 'B','C');
+	$pdf -> Cell(50,10, 'Advert No', 1, 1, 0, 'B','C');
+
+	$pdf -> SetFont('Arial','','8');
+	while ($row = mysqli_fetch_array($results)) {
+		$pdf -> Cell(100,10, $row['designation'] , 1, 0);
+		$pdf -> Cell(30,10, $row['scale'] , 1, 0);
+		$pdf -> Cell(30,10, $row['postsno'] , 1, 0);
+		$pdf -> Cell(50,10, $row['advertno'] , 1, 1);
+	}
+	
+	$pdf -> output();
+
+ ?>
